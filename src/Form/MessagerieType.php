97858f362bbe72fc3285f65faeb49d84b37629ee
gilles.bouchonneau <gilles.bouchonneau@gmail.com>
2021-01-22 19:45:21 +0100

    Adding query_builder in MessagerieType

--- src/Form/MessagerieType.php
+++ src/Form/MessagerieType.php
@@ -4,6 +4,8 @@
 
     use App\Entity\Messagerie;
     use App\Entity\User;
+    use App\Repository\UserRepository;
+    use Doctrine\ORM\EntityRepository;
     use Symfony\Bridge\Doctrine\Form\Type\EntityType;
     use Symfony\Component\Form\AbstractType;
     use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -13,12 +15,23 @@
     use Symfony\Component\Form\Extension\Core\Type\TextType;
     use Symfony\Component\Form\FormBuilderInterface;
     use Symfony\Component\OptionsResolver\OptionsResolver;
+    use Symfony\Component\Security\Core\Security;
     use Symfony\Component\Validator\Constraints\File;
 
     class MessagerieType extends AbstractType
     {
+        private $security;
+
+        public function __construct(Security $security)
+        {
+            $this->security = $security;
+        }
+
         public function buildForm(FormBuilderInterface $builder, array $options)
         {
+            $user = $this->security->getUser();
+            $email = $user->getUsername();
+
             $builder
                 ->add('subject', TextType::class, [
                     'label' => 'Objet',
@@ -32,8 +45,11 @@
                     'label' => 'Destinataire',
                     'attr' => ['class' => 'form-control'],
                     'class' => User::class,
-                    'choice_label' => function ($users) {
-                        return $users->getEmail();
+                    'choice_label' => 'username',
+                    'query_builder' => function (EntityRepository $users) use ($email) {
+                        return $users->createQueryBuilder('u')
+                            ->where('u.email != :email')
+                            ->setParameter('email', $email);
                     }
                 ])
                 ->add('documents', FileType::class, [
